1736cd710297a8ae23f2a55aa59597dabef0e958
Pooja S Kanade <amoghkanade4@gmail.com>
2024-02-16 20:44:32 +0530

    commet hero jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,8 +4,11 @@ const HeroSection = () => {
       <div className="left">
         <h3 className="title1">Where</h3>
         <h1 className="title2">Music</h1>
-        <h5 className="title3">Finds its home</h5>
-        <p className="desc">Explore, Listen and discover your sound</p>
+        <h5 className="title3">finds its home</h5>
+        <p className="desc">
+          Explore, Listen and discover <br />
+          your sound
+        </p>
       </div>
     </div>
   );
